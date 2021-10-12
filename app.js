5c8bdb8f288b6ee5de8f3c9b0a61021d2241de52
ValentinLuengo <valentinluengo2001@gmail.com>
2021-10-11 22:08:24 -0300

    or

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ app.use(express.static(publicPath));
 app.get("/", (req,res) => {res.sendFile(path.join(__dirname, "views/home.html"))});
 app.get("/registro", (req,res) => {res.sendFile(path.join(__dirname, "views/register.html"))});
 app.get("/login", (req,res) => {res.sendFile(path.join(__dirname, "views/login.html"))});
-app.listen(process.env.PORT | 3030,() => {console.log("Mercado Liebre funciona")});
+app.listen(process.env.PORT || 3030,() => {console.log("Mercado Liebre funciona")});
