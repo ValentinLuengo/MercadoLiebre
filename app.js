654e0debb8863c21c3eb792b88db37499ba96f68
ValentinLuengo <valentinluengo2001@gmail.com>
2021-10-11 21:19:00 -0300

    ml1.0

---
+++ app.js
@@ -0,0 +1,10 @@
+const express= require ("express");
+const {homedir}= require("os");
+const path = require("path");
+const app = express();
+const publicPath = path.resolve(__dirname, "./public")
+app.use(express.static(publicPath));
+app.get("/", (req,res) => {res.sendFile(path.join(__dirname, "views/home.html"))});
+app.get("/registro", (req,res) => {res.sendFile(path.join(__dirname, "views/register.html"))});
+app.get("/login", (req,res) => {res.sendFile(path.join(__dirname, "views/login.html"))});
+app.listen(3030,() => {console.log("Mercado Liebre funciona")});
